4df3c45d68e3489393713c111a10d210c9e93d17
chaengs <cathyshim222@naver.com>
2021-01-08 12:02:01 +0900

    새로 올림

--- Step06_Final/src/test/file/dao/FileDao.java
+++ Step06_Final/src/test/file/dao/FileDao.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import test.cafe.dto.CafeDto;
 import test.file.dto.FileDto;
 import test.util.DbcpBean;
 
@@ -21,6 +22,57 @@ public class FileDao {
 		}
 		return dao;
 	}
+	
+	public List<FileDto> getList(FileDto dto){
+		//글목록을 담을 ArrayList 객체 생성
+		List<FileDto> list=new ArrayList<FileDto>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			//select 문 작성
+			String sql = "select *" + 
+					"		from" + 
+					"		    (select result1.*, rownum AS rnum" + 
+					"		    from" + 
+					"		        (select num,writer,title,orgFileName,fileSize,regdate" + 
+					"		        from board_file" + 
+					"		        order by num desc) result1)" + 
+					"		where rnum between ? and ?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 바인딩 할게 있으면 여기서 바인딩한다.
+			pstmt.setInt(1, dto.getStartRowNum());
+			pstmt.setInt(2, dto.getEndRowNum());
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet 으로 부터 data 추출
+			while (rs.next()) {
+				FileDto tmp=new FileDto();
+				tmp.setNum(rs.getInt("num"));
+				tmp.setWriter(rs.getString("writer"));
+				tmp.setTitle(rs.getString("title"));
+				tmp.setOrgFileName(rs.getString("orgFileName"));
+				
+				list.add(dto2);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return list;
+	}
+	
 	//파일 정보를 삭제하는 메소드
 	public boolean delete(int num) {
 		Connection conn = null;
